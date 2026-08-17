f7e713ef402c6b9795e74e5d9a871218316dc454
Lautaro <lautyjoel01@gmail.com>
2026-08-17 19:20:14 -0300

    Configuración de Vercel final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-import db from '@astrojs/db'; // <-- 1. Tiene que estar este import
+import db from '@astrojs/db';
+
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [
-    db() // <-- 2. Tiene que estar agregado acá adentro
+    db()
   ],
+
+  adapter: vercel()
 });
