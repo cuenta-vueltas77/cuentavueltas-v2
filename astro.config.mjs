e6b1cbd6a4c091ac867c532ba683553a94faba5c
Lautaro <lautyjoel01@gmail.com>
2026-08-18 17:00:58 -0300

    Eliminar importacion residual de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
-  
   integrations: [db()]
 });
